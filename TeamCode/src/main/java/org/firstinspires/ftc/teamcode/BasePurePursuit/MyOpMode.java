eca1b1f52a1a6fe9339eb19985cd7e9b0dca4749
yup-its-rowan <roboy13@yahoo.com>
2019-12-08 23:59:28 -0500

    Pure Pursuit Vol 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasePurePursuit/MyOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasePurePursuit/MyOpMode.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.BasePurePursuit;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import java.util.ArrayList;
+
+import static org.firstinspires.ftc.teamcode.BasePurePursuit.RobotMovement.followCurve;
+
 
 public class MyOpMode extends OpMode {
 
@@ -12,6 +16,14 @@ public class MyOpMode extends OpMode {
 
     @Override
     public void loop() {
-        RobotMovement.goToPosition(358.0, 358.0, 0.3);
+
+        ArrayList<CurvePoint> allPoints = new ArrayList<>();
+        allPoints.add(new CurvePoint(0,0,1.0, 1.0, 50,Math.toRadians(50), 1.0));
+        allPoints.add(new CurvePoint(180,180,1.0, 1.0, 50,Math.toRadians(50), 1.0));
+        allPoints.add(new CurvePoint(0,0,1.0, 1.0, 50,Math.toRadians(50), 1.0));
+        allPoints.add(new CurvePoint(0,0,1.0, 1.0, 50,Math.toRadians(50), 1.0));
+
+        followCurve(allPoints, Math.toRadians(90));
+
     }
 }
