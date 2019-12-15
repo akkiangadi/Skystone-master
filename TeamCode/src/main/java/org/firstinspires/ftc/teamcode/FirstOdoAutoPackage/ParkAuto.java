4cca6352a9bfd8e68a9e8c52d283251e455e3ff8
yup-its-rowan <roboy13@yahoo.com>
2019-12-15 14:24:11 -0500

    added buffer functionality to park auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOdoAutoPackage/ParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOdoAutoPackage/ParkAuto.java
@@ -10,16 +10,17 @@ import java.util.ArrayList;
 @Autonomous(name = "ParkAuto", group = "Workers")
 public class ParkAuto extends OpMode {
 
-    DcMotor fl, fr, bl, br;
+    private DcMotor fl, fr, bl, br;
 
-    RobotMovement movement = new RobotMovement();
-    FirstOdo odo = new FirstOdo(null, null, null);
-    ElapsedTime eTime = new ElapsedTime();
+    private RobotMovement movement = new RobotMovement();
+    private FirstOdo odo = new FirstOdo(null, null, null);
+    private ElapsedTime eTime = new ElapsedTime();
+    private double waitTime = 0;
 
-    enum questionaire {question, left, right, leftNear, leftFar, rightNear, rightFar}
-    enum parkAutoPath {leftNear, rightNear, leftFar, rightFar, running}
-    questionaire question = questionaire.question;
-    parkAutoPath autoPath = parkAutoPath.leftNear;
+    private enum questionaire {question, left, right, leftNear, leftFar, rightNear, rightFar}
+    private enum parkAutoPath {leftNear, rightNear, leftFar, rightFar, running}
+    private questionaire question = questionaire.question;
+    private parkAutoPath autoPath = parkAutoPath.leftNear;
 
     @Override
     public void init() {
@@ -37,44 +38,48 @@ public class ParkAuto extends OpMode {
     public void init_loop() {
         switch (question){
             case question:
-                telemetry.addData("which side", "x for left, a for right");
+                telemetry.addData("which side is the robot on", "left for left, right for right");
                 if (eTime.time() > 0.3){
-                    if (gamepad1.a){question = question.right;}
-                    if (gamepad1.x){question = question.left;}
+                    if (gamepad1.dpad_right){question = questionaire.right;}
+                    if (gamepad1.dpad_left){question = questionaire.left;}
                     eTime.reset();
                 }
                 break;
             case left:
-                telemetry.addData("Left, far or near", "x for far, a for near");
+                telemetry.addData("Left, far or near", "up for far, down for near");
                 if (eTime.time() > 0.3){
-                    if (gamepad1.a){question = question.leftNear;}
-                    if (gamepad1.x){question = question.leftFar;}
+                    if (gamepad1.dpad_down){question = questionaire.leftNear;}
+                    if (gamepad1.dpad_up){question = questionaire.leftFar;}
                     eTime.reset();
                 }
                 break;
             case right:
-                telemetry.addData("Right, far or near", "x for far, a for near");
+                telemetry.addData("Right, far or near", "up for far, down for near");
                 if (eTime.time() > 0.3){
-                    if (gamepad1.a){question = question.rightNear;}
-                    if (gamepad1.x){question = question.rightFar;}
+                    if (gamepad1.dpad_down){question = questionaire.rightNear;}
+                    if (gamepad1.dpad_up){question = questionaire.rightFar;}
                     eTime.reset();
                 }
                 break;
             case leftNear:
                 telemetry.addData("left Near", "and i oop");
                 autoPath = parkAutoPath.leftNear;
+                waitTime();
                 break;
             case leftFar:
                 telemetry.addData("left Far", "and i oop");
                 autoPath = parkAutoPath.leftFar;
+                waitTime();
                 break;
             case rightNear:
                 telemetry.addData("right Near", "and i oop");
                 autoPath = parkAutoPath.rightNear;
+                waitTime();
                 break;
             case rightFar:
                 telemetry.addData("right Far", "and i oop");
                 autoPath = parkAutoPath.rightFar;
+                waitTime();
                 break;
         }
     }
@@ -88,9 +93,9 @@ public class ParkAuto extends OpMode {
 
         switch (autoPath){
             case leftFar:
-                path.add(new Waypoint(0,0,2, 1, 1));
+                path.add(new Waypoint(0,0,2,1, 1));
                 path.add(new Waypoint(2,6,2, 0.6, 0.4));
-                path.add(new Waypoint(-4,6,1, 0.4, 0.3));
+                path.add(new Waypoint(-4,6,1, 0.4, 0.3, 0));
                 autoPath = parkAutoPath.running;
                 break;
             case leftNear:
@@ -101,7 +106,7 @@ public class ParkAuto extends OpMode {
             case rightFar:
                 path.add(new Waypoint(0,0,2, 1, 1));
                 path.add(new Waypoint(-2,6,2, 0.6, 0.4));
-                path.add(new Waypoint(44,6,1, 0.4, 0.3));
+                path.add(new Waypoint(44,6,1, 0.4, 0.3, 0));
                 autoPath = parkAutoPath.running;
                 break;
             case rightNear:
@@ -111,6 +116,18 @@ public class ParkAuto extends OpMode {
             case running:
                 telemetry.addData("running", "all gud prolly");
         }
-        movement.followPath(path);
+
+        if (eTime.time() > waitTime){
+            movement.followPath(path);
+        }
+    }
+
+    private void waitTime(){
+        if (eTime.time() > 0.3){
+            if (gamepad1.dpad_down){waitTime -=1;}
+            if (gamepad1.dpad_up){waitTime +=1;}
+            eTime.reset();
+        }
+        telemetry.addData("wait before start", waitTime);
     }
 }
